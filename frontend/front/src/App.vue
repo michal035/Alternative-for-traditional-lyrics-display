7d40889291e78c2cc89b56572de37fd81861cdf4
Michal <mwiselka035@gmail.com>
2023-05-30 09:39:03 +0200

    one page working

---
+++ frontend/front/src/App.vue
@@ -0,0 +1,26 @@
+<script>
+    export default {
+        data(){
+            return{
+                text: 'd'
+            }
+        }
+    }
+
+
+</script>
+
+
+<template>
+    
+    <div>
+        <div class="settings-button">
+            <i class="fas fa-cog"></i>
+        </div>
+
+
+        <h1>{{text}}</h1>
+    </div>
+</template>
+
+
