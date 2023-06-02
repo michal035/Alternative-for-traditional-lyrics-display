e3c3d55887d23719a0f3733bdd9d4d6bab81fc46
Michal <mwiselka035@gmail.com>
2023-06-02 08:24:33 +0200

    API can return simple .docx file -content of .docx in .json format

--- Lyrics_display/api/views.py
+++ Lyrics_display/api/views.py
@@ -2,9 +2,17 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from django.http import HttpResponse
 from django.http import HttpResponseNotFound
-
+from .serializers import ParagraphSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+import docx
+
+
+
+doc = docx.Document('/home/michal/Downloads/Document155.docx')
+paragraphs = [p.text for p in doc.paragraphs]
+
+data = [{'paragraph': paragraph} for paragraph in paragraphs]
 
 
 
@@ -12,6 +20,11 @@ def non(request):
     return HttpResponseNotFound("Token not found")
 
 
+
+
 @api_view(['GET'])
 def re(request, token):
-    return Response(token)
+
+    serializer = ParagraphSerializer(data=data, many=True)
+    serializer.is_valid(raise_exception=True)
+    return Response(serializer.data)
