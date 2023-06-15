0fcd164eadfd1338a16b9c84c8f6edbaede06887
Michal <mwiselka035@gmail.com>
2023-06-15 19:01:32 +0200

    hotfix

---
+++ frontend/front/src/Modal.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="modal">
+    <div class="modal-content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+.modal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5); 
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 9999; 
+}
+
+
+</style>
