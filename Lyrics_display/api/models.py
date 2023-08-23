d10aaa8598f2bb41d88e602ffce9444e8166515b
Michal <mwiselka035@gmail.com>
2023-08-23 19:12:25 +0300

    updated for JWT integration

--- Lyrics_display/api/models.py
+++ Lyrics_display/api/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
 
-class doc(models.Model):
+class User(models.Model):
+    username = models.CharField(max_length=200,null=True, blank=True)
     passwd = models.CharField(max_length=200,null=True, blank=True)
+
+
+
+class Doc(models.Model):
     token = models.CharField(max_length=20)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
